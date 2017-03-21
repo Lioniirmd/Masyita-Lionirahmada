56e68140e0092447b274a1f2b2afee9735d778fe
Lioniirmd <lioni.yuijin@yahoo.com>
2017-03-21 11:45:08 +0700

    Update soal no.1.cpp

--- soal no.1.cpp
+++ soal no.1.cpp
@@ -1,253 +1,205 @@
 #include <iostream>
 #include <stdio.h>
-#include <math.h>
-
 using namespace std;
-
-void awal(){
-	cout<<"\n"<<"=======PROGRAM MENGHITUNG POLINOMIAL======="<<"\n\n";
-	cout<<"\n"<<"Diketahui : "<<"\n";
-	cout<<"\n"<<"P1 = 6x^8 + 8x^7 + 5x^5 + x^3 + 15";
-	cout<<"\n"<<"P2 = 3x^9 + 4x^7 + 3x^4 + 2x^3 + 2x^2 + 10";
-	cout<<"\n"<<"P3 = x^2 + 5"<<"\n";
-	cout<<"\n"<<"Operasi hitung : "<<"\n";
-	cout<<"\n"<<"1.Penjumlahan ";
-	cout<<"\n"<<"2.Pengurangan ";
-	cout<<"\n"<<"3.Perkalian ";
-	cout<<"\n"<<"4.Turunan ";	
-}
-float tambah (int P1[], int P2[], int x){
-	float hasil=0;
-	for(int i=0; i<10; i++)
-	hasil += (P1[i]+P2[i]) * (float) pow(x,i);
-	return hasil;
-}
-
-float kurang (int P1[], int P2[], int x){
-	float hasil=0;
-	for(int i=0; i<10; i++)
-	hasil += (P1[i]-P2[i]) * (float) pow(x,i);
-	return hasil;
-}
-
-float kali(int P1[], int P2[], int x){
-	float hasil=0;
-	for(int i=0; i<10; i++){
-	for(int j=0; j<10; j++)
-	hasil += P1[i] * P2[j] * (float) pow(x,(i+j));
-}
-	return hasil;
-}
-
-float turun(int P1[], int x){
-	float hasil=0;
-	for(int i=0; i<10; i++)
-	hasil = hasil + P1[i] * i * (float) pow(x,i-1);
-	return hasil;
-}
-void pilihan(){
-	int operasi, pil1, pil2, x;
-	float hasil, hasil2;
-	int P1[10] = {15,0,0,1,0,5,0,8,6,0};
-	int P2[10] = {10,0,2,2,3,0,0,4,0,3};
-	int P3[10] = {5,0,1,0,0,0,0,0,0,0};
-
-	do{
-	cout<<"\n"<<"Masukkan Jenis Operasi : ";
-	cin>>operasi;
-	} while(operasi>4 || operasi<1);
-
-	do{
-	cout<<"Pilih Polinomial Pertama : ";
-	cin>>pil1;
-	} while(pil1>3 || pil1<1);
-
-do{
-	cout<<"Pilih Polinomial Kedua : ";
-	cin>>pil2;
-	} while(pil2>3 || pil2<1);
-
-cout<<"Masukkan Nilai x : ";
-cin>>x;
-
-if(operasi==1) {
-switch(pil1)
-{
-case 1 :
-	switch(pil2){
-	case 1 :
-	hasil = tambah(P1,P1,x);
-	break;
-	case 2 :
-	hasil = tambah(P1,P2,x);
-	break;
-	case 3 :
-	hasil = tambah(P1,P3,x);
-	break;
-	}
-	break;
-	
-case 2 :
-	switch(pil2){
-	case 1 :
-		hasil = tambah(P2,P1,x);
-		break;
-	case 2 :
-		hasil = tambah(P2,P2,x);
-		break;
-	case 3 :
-		hasil = tambah(P2,P3,x);
-		break;
-	}
-	break;
-	
-case 3 :
-	switch(pil2){
-	case 1 :
-		hasil = tambah(P3,P1,x);
-		break;
-	case 2 :
-		hasil = tambah(P3,P2,x);
-		break;
-	case 3 :
-		hasil = tambah(P3,P3,x);
-		break;
-	}
-	break;
-}
-}
-
-else if(operasi==2) {
-	switch(pil1){
-	case 1 :
-	switch(pil2){
-		case 1 :
-			hasil = kurang(P1,P1,x);
-			break;
-		case 2 :
-			hasil = kurang(P1,P2,x);
-			break;
-		case 3 :
-			hasil = kurang(P1,P3,x);
-			break;
-		}
-	break;
-
-	case 2 :
-	switch(pil2){
-	case 1 :
-		hasil = kurang(P2,P1,x);
-		break;
-	case 2 :
-		hasil = kurang(P2,P2,x);
-		break;
-	case 3 :
-		hasil = kurang(P2,P3,x);
-		break;
-	}
-	break;
-	
-	case 3 :
-	switch(pil2){
-	case 1 :
-		hasil = kurang(P3,P1,x);
-		break;
-	case 2 :
-		hasil = kurang(P3,P2,x);
-		break;
-	case 3 :
-		hasil = kurang(P3,P3,x);
-		break;
-		}
-	break;
-	}
-}
-
-else if(operasi==3) {
-	switch(pil1){
-	case 1 :
-	switch(pil2){
-	case 1 :
-		hasil = kali(P1,P1,x);
-		break;
-	case 2 :
-		hasil = kali(P1,P2,x);
-		break;
-	case 3 :
-		hasil = kali(P1,P3,x);
-		break;
-	}
-	break;
-	
-case 2 :
-	switch(pil2){
-	case 1 :
-		hasil = kali(P2,P1,x);
-		break;
-	case 2 :
-		hasil = kali(P2,P2,x);
-		break;
-	case 3 :
-		hasil = kali(P2,P3,x);
-		break;
-	}	
-	break;
-	
-case 3 :
-switch(pil2){
-	case 1 :
-		hasil = kali(P3,P1,x);
-		break;
-	case 2 :
-		hasil = kali(P3,P2,x);
-		break;
-	case 3 :
-		hasil = kali(P3,P3,x);
-		break;
-		}
-			break;
-	}
-}
-
-else{
-switch(pil1){
-	case 1 :
-		hasil = turun(P1,x);
-		break;
-	case 2 :
-		hasil = turun(P2,x);
-		break;
-	case 3 :
-		hasil = turun(P3,x);
-		break;
-		}
-		
-		switch(pil2){
-			case 1 :
-				hasil2 = turun(P1,x);
-				break;
-			case 2 :
-				hasil2 = turun(P2,x);
-				break;
-			case 3 :
-				hasil2 = turun(P3,x);
-				break;
-			}
-		}
-
-	if(operasi<4)
-	cout<<"Hasil = %8.2f"<<"\n\n"<<hasil;
-	else
-	{
-	cout<<"Hasil Pertama = %8.2f"<<"\n"<<hasil;
-	cout<<"Hasil Kedua = %8.2f"<<"\n\n"<<hasil2;
-	}
-}
+void Hasil_input(int hasil[], int a);
+void Penjumlahan_polnm(int P1[], int P2[], int a,int b);
+void Pengurangan_polnm(int P1[], int P2[], int a,int b);
+void Perkalian_polnm(int P1[], int P2[], int a,int b);
+void Turunan_polnm(int P1[], int P2[], int a, int b);
+int i,j;
 int main(){
-	awal();
-	pilihan();
-	fflush(stdin);
-	
+	int pol1 , pol2;
+	int pilihan;
+	do{
+		cout<<"\n"<<"==========PROGRAM MENGHITUNG POLINOMIAL=========="<<"\n";
+		cout<<"\n\n"<<"~Jenis Persamaan Polinomial 1(P1)~: ";
+		cout<<"\n"<<"Masukkan Nilai Pangkat terbesar x : ";
+		cin>>pol1;
+		int P1[pol1];
+		cout<<endl<<"\n"<<"Masukkan Nilai Koefisien x :";
+		for(i = 0; i <= pol1 ; i++){
+			cout<<"\n"<<"x ^ = "<<i<<" : ";
+			cin>>P1[i];
+		}
+		cout<<endl<<"\n"<<"Persamaan 1 : ";
+		Hasil_input(P1,pol1);
+		cout<<"\n\n"<<"~Jenis Persamaan Polinomial 2(P2)~: ";
+		cout<<"\n"<<"Masukkan Nilai Pangkat x : ";
+		cin>>pol2;
+		int P2[pol2];
+		cout<<endl<<"\n"<<"Masukkan Nilai Koefisien x";
+		for(i = 0; i <= pol2; i++){
+			cout<<"\n"<<"x ^ = "<<i<<" : ";
+			cin>>P2[i];
+		}
+		cout<<endl<<"\n"<<"Persamaan 2 : ";
+		Hasil_input(P2,pol2);
+			cout<<"\n"<<"Operasi hitung : "<<"\n";
+			cout<<"\n"<<"1.Penjumlahan ";
+			cout<<"\n"<<"2.Pengurangan ";
+			cout<<"\n"<<"3.Perkalian ";
+			cout<<"\n"<<"4.Turunan ";	
+			cout<<"\n"<<"5.Keluar";
+			cout<<"\n"<<"Masukkan pilihan Anda : ";
+			cin>>pilihan;
+		switch(pilihan){
+		case 1:
+			cout<<"\n"<<"Hasil Penjumlahan = ";
+			Penjumlahan_polnm(P1,P2,pol1,pol2);
+			cout<<endl<<endl;
+			cout<<"\n";
+			break;
+		case 2:
+			cout<<"\n"<<"Hasil Pengurangan = ";
+			Pengurangan_polnm(P1,P2,pol1,pol2);
+			cout<<endl<<endl;
+			cout<<"\n";
+			break;
+		case 3:
+			cout<<"\n"<<"Hasil Perkalian = ";
+			Perkalian_polnm(P1,P2,pol1,pol2);
+			cout<<endl<<endl;
+			cout<<"\n";
+			break;
+		case 4:
+			Turunan_polnm(P1,P2,pol1,pol2);
+			cout<<endl<<endl;
+			cout<<"\n";
+			break;
+		case 5:
+				break;
+		default:
+				cout<<"\n"<<"Pilihan Anda Tidak Ada...";
+				cout<<"\n";
+				break;
+		}
+	}
+	while (pilihan != 5);
+	cout<<"============================THANKYOU============================";
+	cout<<endl;
+	system("Pause");
+	return 0;
 }
+void Hasil_input(int hasil[], int a){
+	cout<<endl<<"\n"<<"P1 = ";
+    for(i = 0; i <= a; i++){
+        if(i != a){
+            cout<<hasil[i]<<"x^"<<i<<" + ";
+        }
+        else{
+            cout<<hasil[i]<<"x^"<<i;
+        }
+    }
+}
+void Penjumlahan_polnm(int P1[], int P2[], int a,int b) {
+	int c;
+	int d;
+	if(a <= b){
+		d= b;
+	}
+	else{
+		d= a;
+	}
+	if(d==a)
+	{
+		for(c = b+1; c <= d; c++){
+			P2[c]=0;
+		}
+	}
+	else if(d==b){
+		for(c = a+1; c <= d; c++){
+			P1[c]=0;
+		}
+	}
+	for( i = 0; i <= d;i++){
+		P1[i]=P1[i]+P2[i];
+	}
+	for( i = 0; i<= d;i++){
+		if(i!=d){
+			cout<<P1[i]<<"x^"<<i<<" + ";
+		}
+		else{
+			cout<<P1[i]<<"x^"<<i;
+		}
+	}
+}
+void Pengurangan_polnm(int P1[], int P2[], int a,int b){
+	int c;
+	int d;
+	if(a <= b){
+		d = b;
+	}
+	else{
+		d = a;
+	}
+	if(d==a)
+	{
+		for(c = b+1; c <= d; c++){
+			P2[c]=0;
+		}
+	}
+	else if(d==b){
+		for(c = a+1; c <= d; c++){
+			P1[c]=0;
+		}
+	}
+	for( i = 0; i <= d; i++){
+		P1[i]=P1[i]-P2[i];
+	}
+	for( i = 0; i <= d; i++){
+		if(i!=d){
+			cout<<P1[i]<<"x^"<<i<<" + ";
+		}
+		else{
+			cout<<P1[i]<<"x^"<<i;
+		}
+	}
+}
+void Perkalian_polnm(int P1[], int P2[], int a,int b){
+	int x = 0;
+	int kal1[50], kal2[50];
+	for(i = 0; i <= b; i++){
+		for(j = 0; j <= a; j++){
+			kal1[x] = P2[j] * P1[i];
+			kal2[x] = i + j;
+			x++;
+		}
+	}
+	for( i = 0; i < x; i++){
+		if(i!= x-1){
+			cout<<kal1[i]<<"x^"<<kal2[i]<<" + ";
+		}
+		else{
+			cout<<kal1[i]<<"x^"<<kal2[i];
+		}
+	}
+}
+void Turunan_polnm(int P1[], int P2[], int a, int b){
+	for( i = 0; i <= a; i++){
+		P1[i] = P1[i+1]*(i+1);
+	}
+	cout<<"Turunan P1 = ";
+	for( i = 0; i < a; i++){
+		if(i!= a-1){
+			cout<<P1[i]<<"x^"<<i<<" + ";
+		}
+		else{
+			cout<<P1[i]<<"x^"<<i;
+		}
+	}
+	for( i = 0; i <= b; i++){
+		P2[i]=P2[i+1]*(i+1);
+	}
+	cout<<endl<<"Turunan P2 = ";
+	for( i = 0; i < b; i++){
+		if(i!= b-1){
+			cout<<P2[i]<<"x^"<<i<<" + ";
+		}
+		else{
+			cout<<P2[i]<<"x^"<<i;
+		}
+	}
+}
+		
 
 
